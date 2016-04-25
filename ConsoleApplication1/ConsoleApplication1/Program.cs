47453610b9a6892271bddc3ce679065d20e78744
s.s.belyakov@mail.ru <s.s.belyakov@mail.ru>
2016-04-25 23:33:32 +0300

    исправил на hello World

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hollo word!!!");
+            Console.WriteLine("hello World!!!");
             Console.ReadLine();
         }
     }
