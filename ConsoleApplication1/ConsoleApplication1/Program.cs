67dcf00eb0b75a2d72d9f73222559a3dc004023a
s.s.belyakov@mail.ru <s.s.belyakov@mail.ru>
2016-04-26 23:52:45 +0300

    added list

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,34 +11,33 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            /*p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';*/
             p1.draw();
 
             Point p2 = new Point(3, 5, '#');
-            /*p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';*/
             p2.draw();
 
-            /*int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            draw(x1, y1, sym1);*/
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+            numList.Add(3);
 
-            /*int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            draw(x2, y2, sym2);*/
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
 
             Console.ReadLine();
         }
-
-        /*static void draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }*/
     }
 }
