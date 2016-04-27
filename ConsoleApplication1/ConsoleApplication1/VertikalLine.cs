082748820639f0cf23d0f54b916c5f24fe7b4f45
s.s.belyakov@mail.ru <s.s.belyakov@mail.ru>
2016-04-28 00:08:59 +0300

    added

---
+++ ConsoleApplication1/ConsoleApplication1/VertikalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class VertikalLine
+    {
+        List<Point> pListV;
+
+        public VertikalLine(int x, int yTop, int yBot, char sym)
+        {
+            pListV = new List<Point>();
+            for (int y = yTop; y <= yBot; y++)
+            {
+                Point p3 = new Point(x, y, sym);
+                pListV.Add(p3);
+            }
+                        
+        }
+        public void drow()
+        {
+            foreach(Point p in pListV)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
